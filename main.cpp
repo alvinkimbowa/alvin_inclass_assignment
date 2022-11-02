b2114e8b491e6c4aaec6f0abd164a5c71110e64d
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-11-03 02:59:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ int main()
     //TEST YOUR CODE HERE BY CALLING THE FUNCTION
     // Test Scenarios
     // sample_input1.txt ---> output = 1
-    std::cout << even_num_average("sample_input1.txt") << std::endl;
+    std::cout << count("sample_input1.txt") << std::endl;
 
     // sample_input2.txt ---> output = 1
-    std::cout << even_num_average("sample_input2.txt") << std::endl;
+    std::cout << count("sample_input2.txt") << std::endl;
 
     // sample_input3.txt ---> output = 0
-    std::cout << even_num_average("sample_input3.txt") << std::endl;
+    std::cout << count("sample_input3.txt") << std::endl;
 
     return 0;
 }
